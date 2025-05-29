c5acac323032a5b44e184ad1ab07299f6463b2a8
Patrick Dela Cruz <patrickdeecruz@gmail.com>
2025-05-29 21:11:06 +0800

    Exploring provider way

--- lib/Widgets/chat_input.dart
+++ lib/Widgets/chat_input.dart
@@ -1,8 +1,13 @@
- import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:chat_app/models/chat_message_entity.dart';
+import 'package:chat_app/services/auth_service.dart';
+import 'package:chat_app/widgets/picker_body.dart';
 import 'package:flutter/material.dart';
- class ChatInput extends StatefulWidget {
+import 'package:provider/provider.dart';
+
+class ChatInput extends StatefulWidget {
   final Function(ChatMessageEntity) onSubmit;
- ChatInput({Key? key, required this.onSubmit}) : super(key: key);
+
+  const ChatInput({Key? key, required this.onSubmit}) : super(key: key);
 
   @override
   State<ChatInput> createState() => _ChatInputState();
@@ -10,68 +15,91 @@ import 'package:flutter/material.dart';
 
 class _ChatInputState extends State<ChatInput> {
   final chatMessageController = TextEditingController();
+  String _selectedImageUrl = '';
 
   void onSendButtonPressed() {
-    print('ChatMessage: ${chatMessageController.text}');
-    final newChatMessage = ChatMessageEntity(
-      text: chatMessageController.text,
-      id: '8',
-      createdAt: DateTime.now().millisecondsSinceEpoch,
-      author: Author(username: 'Elton Bernil'),
-      imageUrl: ''
-    );
+    if (chatMessageController.text.trim().isEmpty && _selectedImageUrl.isEmpty)
+      return;
 
+    final newChatMessage = ChatMessageEntity(
+      text: chatMessageController.text.trim(),
+      id: '244',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      author: Author(username: context.read<AuthService>().getUserName()),
+      imageUrl: _selectedImageUrl,
+    );
 
     widget.onSubmit(newChatMessage);
     chatMessageController.clear();
+    _selectedImageUrl = '';
+    setState(() {});
   }
 
+  void onImagePicked(String newImageUrl) {
+    setState(() {
+      _selectedImageUrl = newImageUrl;
+    });
+    Navigator.of(context).pop();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-                height: 100,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    IconButton(
-                      onPressed: () {}, 
-                      icon: Icon(Icons.add, 
-                      color: Colors.white,
-                      )
-                    ),
-
-                    Expanded(
-                      child: TextField(
-                        keyboardType: TextInputType.multiline,
-                        maxLines: 5,
-                        minLines: 1,
-                        controller: chatMessageController,
-                        textCapitalization: TextCapitalization.sentences,
-
-                      style: TextStyle(color: Colors.white),
-                      decoration: InputDecoration(
-                        hintText: "Type a message",
-                        hintStyle: TextStyle(color: Colors.blueGrey),
-                        border: InputBorder.none,
-                      ),
-                    )),
-
-                    IconButton(
-                      onPressed: onSendButtonPressed, 
-                      icon: Icon(
-                        Icons.send,
-                        color: Colors.white,
-                      ),
-                    ),
-
-                    ],
-                ),
-
-                decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-
-                ),
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+      decoration: const BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {
+              showModalBottomSheet(
+                context: context,
+                builder: (BuildContext context) {
+                  return NetworkImagePickerBody(
+                    onImageSelected: onImagePicked,
+                  );
+                },
               );
+            },
+            icon: const Icon(Icons.add, color: Colors.white),
+          ),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  keyboardType: TextInputType.multiline,
+                  maxLines: 5,
+                  minLines: 1,
+                  controller: chatMessageController,
+                  textCapitalization: TextCapitalization.sentences,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    hintText: "Type your message",
+                    hintStyle: TextStyle(color: Colors.blueGrey),
+                    border: InputBorder.none,
+                  ),
+                ),
+                if (_selectedImageUrl.isNotEmpty)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Image.network(
+                      _selectedImageUrl,
+                      height: 50,
+                    ),
+                  ),
+              ],
+            ),
+          ),
+          IconButton(
+            onPressed: onSendButtonPressed,
+            icon: const Icon(Icons.send, color: Colors.white),
+          ),
+        ],
+      ),
+    );
   }
+}
