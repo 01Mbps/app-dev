89e566b1651bb5df3cc707f3d7c42768574f5eb4
Patrick Dela Cruz <patrickdeecruz@gmail.com>
2025-05-29 10:40:21 +0800

    Validating User Inputs

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,86 +1,136 @@
 import 'package:flutter/material.dart';
 
-
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-void loginUser() {
-    print('Login successful');
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  final userNameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void loginUser() {
+    if (_formKey.currentState != null && _formKey.currentState!.validate()) {
+      print(userNameController.text);
+      print(passwordController.text);
+      print('Login successful');
+    } else {
+      print('Login failed');
+    }
+  }
+
+  @override
+  void dispose() {
+    userNameController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-
-            children: [
-              const Text(
-                'Let\'s sign you in!',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const Text(
+                  'Let\'s sign you in!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
 
-
-              const Text(
-                'Welcome back!\nYou\'ve been missed!',
-
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 20,
-                  color: Colors.blueGrey,
+                const Text(
+                  'Welcome back!\nYou\'ve been missed!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                    color: Colors.blueGrey,
+                  ),
                 ),
-              ),
+                const SizedBox(height: 20),
 
-              Image.network(
-                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
-                height: 200,
-              ),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+                const SizedBox(height: 24),
 
-            ElevatedButton(
-              onPressed: loginUser, 
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: userNameController,
+                        decoration: const InputDecoration(
+                          hintText: 'Add your username',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder(),
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter your username';
+                          } else if (value.length < 5) {
+                            return 'Your username must be at least 5 characters long';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 24),
 
-              child: Text(
-              'Click Me' , 
-              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w300),
-              )),
-                InkWell(
-              splashColor: Colors.red,
-              onDoubleTap: () {
-                print('Double Tapped');
-              },
+                      TextFormField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: 'Type your Password',
+                          hintStyle: TextStyle(color: Colors.blueGrey),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
 
-              onLongPress: () {
-                print('Long Pressed');
-              },
+                const SizedBox(height: 24),
 
-              onTap: () {
+                ElevatedButton(
+                  onPressed: loginUser,
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.w300),
+                  ),
+                ),
 
-                print('Linked Clicked');
-              },
-              child: Column(
-                children: [
-                  Text('Find us on'),
-                  Text('https://patrickCr8s.com'),
-                ],
-              ),
+                const SizedBox(height: 20),
+
+                GestureDetector(
+                  onDoubleTap: () {
+                    print('Double Tapped');
+                  },
+                  child: Column(
+                    children: const [
+                      Text('Find us on'),
+                      Text('https://patrickCr8s.com'),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            ],
-
           ),
-          ),
+        ),
       ),
     );
   }
+}
